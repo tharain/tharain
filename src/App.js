2b23855ab88198b46125ab38507fd874b989a3fd
Won <tharain@users.noreply.github.com>
2018-05-01 16:22:27 +0800

    Added General Contact to header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Carousel } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Carousel className="App-header">
+          <Carousel.Item>
+            <h1>❤ Daphne Won ❤</h1>
+            <p>❤ Bachelor in Computer Science (National University of Singapore) ❤</p>
+            <p>❤ 8163 7781 ❤ won.jr@hotmail.com ❤</p>
+          </Carousel.Item>
+          <Carousel.Item>
+
+          </Carousel.Item>
+          <Carousel.Item>
+          
+          </Carousel.Item>
+        </Carousel>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
