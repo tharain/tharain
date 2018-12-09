925d540c24cd5e763c11f4520c540675c7d30f57
Won <tharain@users.noreply.github.com>
2018-12-09 15:12:44 +0800

    Updates

---
+++ precache-manifest.16130508ad16cf8cca9e90b78aad70ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc80695ca728fb49baf604dba8126fbd",
+    "url": "/tharain/static/media/shopee_logo.dc80695c.svg"
+  },
+  {
+    "revision": "742fa59d6924dce46f1824d7e8573ed3",
+    "url": "/tharain/static/media/hp_logo.742fa59d.png"
+  },
+  {
+    "revision": "fb3925841b7f9d979289fb993ecdc09f",
+    "url": "/tharain/static/media/astar_logo.fb392584.svg"
+  },
+  {
+    "revision": "3739de73b206078d1ec5",
+    "url": "/tharain/static/js/runtime~main.3739de73.js"
+  },
+  {
+    "revision": "1234d086f72137ee2f5c",
+    "url": "/tharain/static/js/main.1234d086.chunk.js"
+  },
+  {
+    "revision": "8e5c457c4fc93d4954f2",
+    "url": "/tharain/static/js/1.8e5c457c.chunk.js"
+  },
+  {
+    "revision": "1234d086f72137ee2f5c",
+    "url": "/tharain/static/css/main.7fbe2d55.chunk.css"
+  },
+  {
+    "revision": "d01e89667e8549694a253f770206a31e",
+    "url": "/tharain/index.html"
+  }
+];
